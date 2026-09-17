ed1ef86239e51e44db2c7efb3dbd3d0965228620
Oumaima <mahfoudeoumaima@gmail.com>
2026-09-17 16:28:17 +0100

    css

--- prototype/code/index.php
+++ prototype/code/index.php
@@ -7,6 +7,7 @@ require_once'requet.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gestion de produits</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Gestion de produits</h1>
